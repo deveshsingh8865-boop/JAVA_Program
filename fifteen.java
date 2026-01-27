67b254ad3a9f39e74dc3cd0d21f24bc4d2c0c350
deveshsingh8865-boop <deveshsingh.8865@gmail.com>
2026-01-27 16:26:35 +0530

    Add files via upload

---
+++ fifteen.java
@@ -0,0 +1,10 @@
+public class fifteen {
+    public static void main(String[] args) {
+        int a =10,b,c;
+        c = ++a;
+        b = a+++c--;
+        System.out.println(a++ +","+ b++ +","+ ++c);
+    }
+    
+}
+//  it is answer of this question 12,22,11
